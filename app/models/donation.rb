91f7cbab2341bcf247c18356122a9edcf39ddaef
Jeremy Green <jeremy@octolabs.com>
2014-09-22 23:01:15 -0500

    Process payments and use guids in URLs

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,9 +1,30 @@
 class Donation < ActiveRecord::Base
 
+  def to_param
+    guid
+  end
+
+  # Ideally the process of charging a card would happen
+  # in the bakground and in a service object of some sort.
+  # This is the quick and dirty method that doesn't require background workers.
+  after_create :charge_the_token
+  def charge_the_token
+    charge = Stripe::Charge.create(
+      amount:      (amount * 100).to_i,
+      currency:    "usd",
+      card:        token_id,
+      description: email
+    )
+  end
+
   before_create :populate_guid
+
   private
   def populate_guid
     self.guid = SecureRandom.uuid()
   end
+
+
+  
 
 end
