32c788139b09b3a6567ce2e91baf782b9f350477
Jeremy Green <jeremy@octolabs.com>
2014-09-23 15:43:52 -0500

    Remove some routes we don't need

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :donations
+  resources :donations, :except => [:edit,:update,:delete]
   root 'donations#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
