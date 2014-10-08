a30bb0b94ea93497abbc110e55cea2792f587bc3
rob sullivan <rob@datachomp.com>
2014-10-07 21:48:00 -0500

    donation mailer preview

--- test/mailers/previews/donation_mailer_preview.rb
+++ test/mailers/previews/donation_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/donation_mailer
 class DonationMailerPreview < ActionMailer::Preview
 
+  def thank_you_email
+    @donation = Donation.first
+    DonationMailer.thank_you_email(@donation)
+  end
+
 end
