e6c048e915fd37131b88cff0a59f43f68ee82713
rob sullivan <rob@datachomp.com>
2014-10-07 21:48:54 -0500

    clarified preview name

--- test/mailers/previews/donation_mailer_preview.rb
+++ test/mailers/previews/donation_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/donation_mailer
 class DonationMailerPreview < ActionMailer::Preview
 
-  def thank_you_email
+  def thank_you_email_of_first_doner
     @donation = Donation.first
     DonationMailer.thank_you_email(@donation)
   end
