c08ab3481f442379d77d059fd94f35fff3ca670e
Jeremy Green <jeremy@octolabs.com>
2016-02-21 00:44:07 -0600

    Import styles from html5up-telephasic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
   def cents_to_dollars(cents)
     number_to_currency(cents.to_f/100)
   end
+
+  def add_page_class klass
+    @page_classes ||= []
+    @page_classes.push klass
+  end
+
+  def page_class
+    (@page_classes || []).join(" ")
+  end
+
 end
