1a7ef98426bab36b1e550aaa69f06b34386950dc
Jeremy Green <jeremy@octolabs.com>
2017-03-03 11:02:31 -0600

    More tests and clean up of the StripeInvoicePaymentSucceeded service

--- app/services/stripe_invoice_payment_succeeded.rb
+++ app/services/stripe_invoice_payment_succeeded.rb
@@ -1,6 +1,5 @@
 class StripeInvoicePaymentSucceeded
   def call(event)
-    puts "------------ handling event in StripeInvoicePaymentSucceeded ------------"
     Rails.logger.debug "Stripe Event: #{event.to_json}"
     donation = create_donation_from_event(event)
 
@@ -17,42 +16,20 @@ class StripeInvoicePaymentSucceeded
 
   def create_donation_from_event(event)
     invoice = event.data.object
-
-    return unless invoice.charge
-
     subscription = Subscription.find_by!(stripe_subscription_id: invoice.subscription)
-
-    #stripe_sub = Stripe::Customer.retrieve(subscription.stripe_customer_id).subscriptions.retrieve(invoice.subscription)
-
-    #subscription.sync_with!(stripe_sub)
-
     donation = create_donation(subscription, invoice)
-
-    #charge = Stripe::Charge.retrieve(invoice.charge, access_token)
-
-    #update_donation_with_charge(donation, charge, access_token)
-
     return donation
+  rescue ActiveRecord::RecordNotFound
+    # we couldn't find a subscription to match up with this invoce
+    return nil
   end
 
   def create_donation(subscription, invoice)
     Donation.new do |d|
       d.email = subscription.email
-      #d.livemode = subscription.livemode
       d.subscription_id = subscription.id
-      #s.state = 'processing'
-      #s.owner = subscription
-      #s.product = subscription.plan
       d.token_id = 'invoice'
       d.amount = invoice.total.to_f / 100 # we store decimal
-      #d.currency = invoice.currency
     end
   end
-
-  #def update_donation_with_charge(donation, charge, secret_key)
-    #donation.stripe_id  = charge.id
-    #donation.card_type  = charge.source.brand
-    #donation.card_last4 = charge.source.last4
-  #end
-
 end
--- spec/requests/stripe_webhooks_spec.rb
+++ spec/requests/stripe_webhooks_spec.rb
@@ -23,11 +23,27 @@ describe "Billing Events" do
       create :subscription
     end
 
-    it "is successful" do
+    it "returns 200 and creates a donation" do
       post '/stripe_webhooks', id: 'invoice_payment_succeeded'
       expect(response.code).to eq "200"
       expect(Donation.count).to eq 1
       # Additional expectations...
     end
   end
+
+  describe "invoice.payment.succeeded without a subscription" do
+    before do
+      expect_any_instance_of(Subscription).to receive(:charge_the_token).and_return(nil)
+      stub_stripe_event 'invoice_payment_succeeded'
+      create :subscription, :stripe_subscription_id => 'sub_does_not_exist'
+    end
+
+    it "returns 200 and does not create a donation" do
+      post '/stripe_webhooks', id: 'invoice_payment_succeeded'
+      expect(response.code).to eq "200"
+      expect(Donation.count).to eq 0
+      # Additional expectations...
+    end
+  end
+
 end
