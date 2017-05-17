0600f9724e04a8b2aa811909eab3d29e96674b24
Jeremy Green <jeremy@octolabs.com>
2017-05-17 11:34:18 -0500

    New value for let's encrypt

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,5 +1,9 @@
 class LetsencryptController < ApplicationController
   def challenge
+<<<<<<< Updated upstream
     render text: 'sb1SiyhA0ZOuWlBidZ0GTik1mWnkR0tmvn0DTRHbR34.jAGCbyEGmhGUoygau6KsU64SmHCdKi_XbyD23BsQjF4'
+=======
+    render text: 'fMg7u1mDnstJQ5zY6FVl3DTIX8OLhPKroGtvFYoddQc.jAGCbyEGmhGUoygau6KsU64SmHCdKi_XbyD23BsQjF4'
+>>>>>>> Stashed changes
   end
 end
