5ecdb3dfb86a7b8451ec42e54df970514fb33fa9
Jeremy Green <jeremy@octolabs.com>
2017-12-30 14:50:01 -0600

    Capture the start/end times for the roll up

--- app/services/donation_roll_up_creator.rb
+++ app/services/donation_roll_up_creator.rb
@@ -5,7 +5,9 @@ class DonationRollUpCreator
       .group("email")
       .where("created_at >= ? and created_at < ?", start_time, end_time)
     data.each do |roll_up_data|
-      DonationRollUp.create!(roll_up_data.attributes.except(:id))
+      attrs = roll_up_data.attributes.except(:id)
+      attrs = attrs.merge(start_time: start_time, end_time: end_time)
+      DonationRollUp.create!(attrs)
     end
   end
 end
