2e1f56ea67b57b3518a35988d9a0bd990971df00
Jeremy Green <jeremy@octolabs.com>
2018-01-02 17:39:16 -0600

    Update the service for creating coupons at Ecwid

--- app/services/ecwid_discount_coupon_creator.rb
+++ app/services/ecwid_discount_coupon_creator.rb
@@ -2,14 +2,17 @@ class EcwidDiscountCouponCreator
   def self.call(donation_roll_up)
     client = EcwidApi::Client.new(ENV['ECWID_STORE_ID'], ENV['ECWID_ACCESS_TOKEN'])
     params = {
-      "name": "2017 Membership Thank You for #{donation_roll_up.email} - #{donation_roll_up.plan_name}",
-      "code": donation_roll_up.ecwid_discount_coupon_code,
-      "discountType": "ABS_AND_SHIPPING",
-      "status": "ACTIVE",
-      "discount": donation_roll_up.ecwid_discount_coupon_amount,
-      "launchDate": "2018-01-01",
-      "usesLimit": "SINGLE",
-      "repeatCustomerOnly": false,
+      name: "2017 Membership Thank You for #{donation_roll_up.email} - #{donation_roll_up.plan_name}",
+      code: donation_roll_up.ecwid_discount_coupon_code,
+      discountType: 'ABS_AND_SHIPPING',
+      status: 'ACTIVE',
+      discount: donation_roll_up.ecwid_discount_coupon_amount.to_i,
+      usesLimit: 'SINGLE',
+      repeatCustomerOnly: false,
+      catalogLimit:{
+        products:[],
+        categories:[25486506]
+      }
     }
     response = client.post("discount_coupons", params)
     donation_roll_up.update_attributes({
